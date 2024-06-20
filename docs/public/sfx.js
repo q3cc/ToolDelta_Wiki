a8309901ec88461e933a8078d021016605c1df3a
SuperScript-PRC <2528622340@qq.com>
2024-06-20 20:22:30 +0800

    sfx test 2

--- docs/public/sfx.js
+++ docs/public/sfx.js
@@ -1,8 +1,12 @@
-const aud = new Audio("sfx_src/sfx_intro.wav")
 function play_sfx_intro() {
-    alert("testing")
-    aud.play()
-    aud.loop = false
+    try {
+        const aud = new Audio("sfx_src/sfx_intro.wav")
+        aud.play()
+        aud.loop = false
+        alert("ok")
+    } catch (e) {
+        alert(e)
+    }
 }
 
 play_sfx_intro()
