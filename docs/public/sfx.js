2957fe81d81003f3dbef976fca5eff6874342fc7
SuperScript-PRC <2528622340@qq.com>
2024-06-20 20:47:42 +0800

    sfx test 5

--- docs/public/sfx.js
+++ docs/public/sfx.js
@@ -1,6 +1,6 @@
 function play_sfx_intro() {
     try {
-        const aud = new Audio("./sfx_src/sfx_intro.mp3")
+        const aud = new Audio("https://tdload.tblstudio.cn/raw.githubusercontent.com/ToolDelta/Wiki/main/sfx_src/sfx_intro.mp3")
         aud.play()
         aud.loop = false
         alert("ok")
