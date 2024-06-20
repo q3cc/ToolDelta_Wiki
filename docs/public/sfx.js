d341db44756d7ed312e975f0cf7aa1bf44f2f95d
SuperScript-PRC <2528622340@qq.com>
2024-06-20 20:33:11 +0800

    sfx test 3

--- docs/public/sfx.js
+++ docs/public/sfx.js
@@ -1,6 +1,6 @@
 function play_sfx_intro() {
     try {
-        const aud = new Audio("sfx_src/sfx_intro.wav")
+        const aud = new Audio("./sfx_src/sfx_intro.wav")
         aud.play()
         aud.loop = false
         alert("ok")
